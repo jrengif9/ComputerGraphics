df6f54d9dbda7f44ac2a87bbac3eacc98c7abc8c
jrengif9 <jrengif9@eafit.edu.co>
2019-08-01 17:26:34 -0500

    Starting project - Line Algorithms

---
+++ Line_Drawing_Algorithms/LineDrawingAlgorithms/LineDrawingAlgorithms/Main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+/*Libreria usada solo en Windows - debe ir antes que las demás librerias*/
+#include<windows.h>
+/*Libreria del OpenGL*/
+#include<GL/glut.h>
+
+
+using namespace sf;
+
+int main(int argc, char** argv)
+
+{
+
+}
