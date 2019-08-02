1aa8fc9db0b61b16d02b3bd87196622451eb433c
jrengif9 <jrengif9@eafit.edu.co>
2019-08-01 19:29:43 -0500

    Adding functional DDA algorithm

--- Line_Drawing_Algorithms/LineDrawingAlgorithms/LineDrawingAlgorithms/Main.cpp
+++ Line_Drawing_Algorithms/LineDrawingAlgorithms/LineDrawingAlgorithms/Main.cpp
@@ -7,8 +7,73 @@
 
 using namespace sf;
 
-int main(int argc, char** argv)
+#include<stdlib.h>
+#include<stdio.h>
 
+float xInit, xEnd, yInit, yEnd;
+
+void display(void)
 {
+	float dy, dx, step, x, y, k, Xin, Yin;
+	dx = xEnd - xInit;
+	dy = yEnd - yInit;
 
+	if (abs(dx) > abs(dy))
+	{
+		step = abs(dx);
+	}
+	else
+		step = abs(dy);
+
+	Xin = dx / step;
+	Yin = dy / step;
+
+	x = xInit;
+	y = yInit;
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+
+	for (k = 1; k <= step; k++)
+	{
+		x = x + Xin;
+		y = y + Yin;
+
+		glBegin(GL_POINTS);
+		glVertex2i(x, y);
+		glEnd();
+	}
+
+
+	glFlush();
+}
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-100, 100, -100, 100);
+}
+
+int main(int argc, char** argv) {
+	printf("Enter the value of xInit : ");
+	scanf_s("%f", &xInit);
+	printf("Enter the value of yInit : ");
+	scanf_s("%f", &yInit);
+	printf("Enter the value of xEnd : ");
+	scanf_s("%f", &xEnd);
+	printf("Enter the value of yEnd : ");
+	scanf_s("%f", &yEnd);
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("DDA Line Algo");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
 }
